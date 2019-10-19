e63229d9f912b7484b30b677a8e61773cc28aa77
lamaran1 <56564009+lamaran1@users.noreply.github.com>
2019-10-19 16:52:52 +0100

    Update models.py

--- eHealth/signup/models.py
+++ eHealth/signup/models.py
@@ -27,19 +27,5 @@ def create_user_profile(sender, instance, created, **kwargs):
 post_save.connect(create_user_profile, sender=User)
 
 
-#class Profile(models.Model):
- #   choices = (("Yes", 'Yes'), ("No", 'No'))
-    #user = models.OneToOneField(User, on_delete=models.CASCADE)
-   # image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-  #  Email = models.EmailField(default='none@email.com')
-   # Malaria = models.CharField(max_length=255, choices=choices )
-   # Diarrheal_Diseases = models.CharField(max_length=255,choices=choices)
-   # Road_Injuries = models.CharField(max_length=255, choices=choices)
-   # Tuberculosis = models.CharField(max_length=255, choices=choices)
-   # Cough = models.CharField(max_length=255, choices=choices)
-    #def __str__(self):
-    #    return f'{self.user.username} Profile'
-
-
 def __str__(self):
     return self.Name
